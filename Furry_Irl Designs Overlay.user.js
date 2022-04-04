98cd1d9fc1065bffee3beada9fb50d70136628cf
Ziray-a <102958815+Ziray-a@users.noreply.github.com>
2022-04-04 19:10:52 +0200

    Update Furry_Irl Designs Overlay.user.js

--- Furry_Irl Designs Overlay.user.js
+++ Furry_Irl Designs Overlay.user.js
@@ -9,6 +9,26 @@
 // @license      GPLv3
 // @grant        none
 // ==/UserScript==
+
+function createImage(){
+    document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
+            (function () {
+                const i = document.createElement("img");
+                i.src = "https://raw.githubusercontent.com/Furryirlburner1/Furryirloverlaysync/main/reference.png";
+                i.onload = () => {
+                    if (i.width === i.height) {
+                        i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 2000px;";
+                    } else {
+                        i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 1000px;";
+                    }
+                };
+                return i;
+            })())
+}
+
+function destroyImage(){
+    document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].removeChild(document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].lastElementChild);
+}
 if (window.top !== window.self) {
     window.addEventListener('load', () => {
         document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
@@ -26,3 +46,12 @@ if (window.top !== window.self) {
             })())
     }, false);
 }
+
+function myFunction () {
+    //console.dir(document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0]);
+    destroyImage();
+    createImage();
+    console.log("updated image");
+}
+
+var interval = setInterval(function () { myFunction(); }, 60000);
